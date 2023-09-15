f7b97b960b22ca6180d77c2f39fefb2c9a9a12df
lo3elia <ssdf8876@naver.com>
2023-09-15 14:18:35 +0900

    Add C file [hw1.c]

---
+++ hw1.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 0, b = 0;
+	scanf("Input two integers: %d %d \n", &a, &b);
+	printf("10 & 15 = %d \n", a & b);
+	printf("10 | 15 = %d \n", a | b);
+	printf("10 ^ 15 = %d \n", a ^ b);
+	return 0;
+}
