a1cc72827e76c25c4f74563e0f3edf22e3f67705
lo3elia <96883999+lo3elia@users.noreply.github.com>
2023-09-27 16:10:23 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -1,9 +1,11 @@
-﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNNINGS
+#include <stdio.h>
 
 int main(void)
 {
 	int a = 0, b = 0;
-	scanf("Input two integers: %d %d \n", &a, &b);
+	printf("Input two integers: ");
+	scanf("%d %d", &a, &b);
 	printf("10 & 15 = %d \n", a & b);
 	printf("10 | 15 = %d \n", a | b);
 	printf("10 ^ 15 = %d \n", a ^ b);
