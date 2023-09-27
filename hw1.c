151879da9d71e6110ccdb1141823f0be2134867e
lo3elia <96883999+lo3elia@users.noreply.github.com>
2023-09-27 16:11:36 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -6,8 +6,8 @@ int main(void)
 	int a = 0, b = 0;
 	printf("Input two integers: ");
 	scanf("%d %d", &a, &b);
-	printf("10 & 15 = %d \n", a & b);
-	printf("10 | 15 = %d \n", a | b);
-	printf("10 ^ 15 = %d \n", a ^ b);
+	printf("%d & %d = %d \n", a, b, a & b);
+	printf("%d | %d = %d \n", a, b, a | b);
+	printf("%d ^ %d = %d \n", a, b, a ^ b);
 	return 0;
 }
